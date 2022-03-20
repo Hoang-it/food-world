78adc4fb3f24e3e39a106f1f804cc00115eb0702
hoang nguyen huu <hoang.nguyenhuu@lotussoft.vn>
2022-03-20 23:42:06 +0700

    [SPRINT 1] Init project

---
+++ src/main/java/hoang/fw/foodworld/FoodWorldApplication.java
@@ -0,0 +1,15 @@
+package hoang.fw.foodworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class FoodWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodWorldApplication.class, args);
+    }
+}
